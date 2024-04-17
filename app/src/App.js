cf16f5692433c62f7a35b1cb5ad4b07d763968f3
akann0 <akann@ucsd.edu>
2024-04-17 16:11:04 -0700

    api call works

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [jobData, setJobData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const json = await callApi();
+      setJobData(json);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <h1>Jobs</h1>
+        <ul>
+          {jobData.map(job => (
+            <li key={job.id}>{job.title}</li>
+          ))}
+        </ul>
+      </body>
     </div>
   );
+}
+
+function callApi() {
+  return fetch('https://62bc8d086b1401736cfcd8fb.mockapi.io/jobs')
+    .then(response => response.json())
+    .then(data => {
+      console.log("data", data);
+      return data;
+    });
 }
 
 export default App;
